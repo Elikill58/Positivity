da7d8f934d37d286cdb48be36e319fa6ac3c4679
Alexmdz77 <alext77165@gmail.com>
2021-05-18 10:02:14 +0200

    add page-wrapper & content-wrapper

--- bans.php
+++ bans.php
@@ -12,23 +12,27 @@ require_once './include/page.php';
     <link href="./include/css/main.css" rel="stylesheet">
 </head>
 <body>
-    <?php
+	<div class="page-wrapper">
+        <?php
+            $page = new Page("ban");
+            $page->show_header();
+        ?>
+		<div class="content-wrapper">
+			<div class="container">
+                <?php
+                    $rows = $page->run_query();
+                    foreach ($rows as $row) {
+                        $player_name = $page->get_name($row["id"]);
+                        if ($player_name === null)
+                            continue;
+                        $page->print_row($row);
+                    }
 
-    $page = new Page("ban");
-
-    $page->show_header();
-
-    $rows = $page->run_query();
-    foreach ($rows as $row) {
-        $player_name = $page->get_name($row["id"]);
-        if ($player_name === null)
-            continue;
-        $page->print_row($row);
-    }
-
-    $page->show_page_mover();
-
-    $page->show_footer();
-    ?>
+                    $page->show_page_mover();
+                ?>
+            </div>
+        </div>
+        <?php $page->show_footer(); ?>
+    </div>
 </body>
 </html>
