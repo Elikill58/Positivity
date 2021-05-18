fb70a8fcbb2b387c18376bc98adecb9928ff53b8
Alexmdz77 <alext77165@gmail.com>
2021-05-18 10:12:28 +0200

    fix footer

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,3 @@
-			</table>
-        </div>
-    </div>
-</div>
-<script src="./include/js/jquery-3.3.1.min.js"></script>
-<script src="./include/js/bootstrap.min.js"></script>
 <footer>
 	<p class="text-light text-center" style="margin-bottom: 0">
 		<a href="https://www.spigotmc.org/resources/48399/">&copy; Negativity - Elikill58 & RedNesto - Redesigned by Alexmdz77</a>
