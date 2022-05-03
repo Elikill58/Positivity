2bcc5a3dbb5ac183f1359ea158bc3b178ba26a58
Elikill58 <arpetzouille@gmail.com>
2022-05-03 10:22:09 +0200

    Update errors pages

---
+++ error/already-config.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Negativity - Already config</title>
+    <link href="../include/css/main.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container solo">
+        <h2>Already config</h2>
+        <p>
+            Welcome to Positivity, the Negativity web interface!
+            <br>
+            This website is already configurated ! Just go on main page and use it :)
+        </p>
+        <br>
+        <a href="../" class="btn-outline"><div class="text">Continue</div></a>
+    </div>
+</body>
+</html>
+
+<style type="text/css">
+
+</style>
