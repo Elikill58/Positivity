cc71b50bf31c08a96efb729bbf5db6d4625d0471
Elikill58 <arpetzouille@gmail.com>
2022-05-03 10:19:32 +0200

    Update outdated plugin page

--- error/outdated-plugin.php
+++ error/outdated-plugin.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <title>litebans-php - Outdated Plugin</title>
+    <title>Negativity - Outdated Plugin</title>
     <link href="../include/css/main.css" rel="stylesheet">
 </head>
 <body>
@@ -12,13 +12,13 @@
     <div class="jumbotron">
         <h2>Outdated Plugin</h2><br>
         <div class="text-warning">
-            The web interface has connected to the database, but this database is managed by an older version of LiteBans.
+            The web interface has connected to the database, but this database is managed by an older version of Negativity.
             <br>
-            You will need to update LiteBans before you can continue using the web interface. Older versions are not supported.
+            You will need to update Negativity before you can continue using the web interface.
             <br>
-            Updates can be downloaded here: https://www.spigotmc.org/resources/litebans.3715/
+            Updates can be downloaded here: https://www.spigotmc.org/resources/86874/
             <br>
-            LiteBans will automatically upgrade the tables in the database after you have updated. After this, the web interface will work as normal.
+            Negativity will automatically upgrade the tables in the database after you have updated. After this, the web interface will work as normal.
         </div>
         <br>
         <a href="../" class="btn btn-default">Try Again</a>
